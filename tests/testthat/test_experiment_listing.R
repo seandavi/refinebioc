362272ddc6b1075e5ea4516468f60ee434e78a89
Sean Davis <seandavi@gmail.com>
2024-01-15 15:16:44 -0500

    tests for experiment_listing

---
+++ tests/testthat/test_experiment_listing.R
@@ -0,0 +1,18 @@
+TEST_PAGE_COUNT <- 2
+
+test_result <- experiment_listing(.pages = TEST_PAGE_COUNT)
+
+test_that("Should return a tibble", {
+  expect_s3_class(test_result, "tbl_df")
+})
+
+test_that(sprintf(
+  "Should return a tibble with %d rows",
+  TEST_PAGE_COUNT * 1000
+), {
+  expect_equal(nrow(test_result), TEST_PAGE_COUNT * 1000)
+})
+
+test_that("Should return a tibble with 21 columns", {
+  expect_equal(ncol(test_result), 21)
+})
