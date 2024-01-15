c3848c9073b08db31a1c2fd0047792bfc0de958d
Sean Davis <seandavi@gmail.com>
2024-01-15 15:40:52 -0500

    Remove some old datastore_path code

--- R/datastore_path.R
+++ R/datastore_path.R
@@ -10,16 +10,15 @@
 #'
 #' @export
 datastore_get_path <- function() {
-  if (!missing(datastore_path)) {
-    options(refinebioc.datastore = datastore_path)
-  } else {
-    datastore_path <- file.path(tempdir(), "rb_datastore")
-  }
+  datastore_path <- NULL
   if (!is.null(getOption("refinebioc.datastore"))) {
     datastore_path <- getOption("refinebioc.datastore")
   }
   if (Sys.getenv("REFINEBIOC_DATASTORE", "") != "") {
     datastore_path <- Sys.getenv("REFINEBIOC_DATASTORE")
+  }
+  if (is.null(datastore_path)) {
+    datastore_path <- file.path(tempdir(), "rb_datastore")
   }
   if (!dir.exists(datastore_path)) {
     dir.create(datastore_path, recursive = TRUE)
