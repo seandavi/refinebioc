136d4eb17bf862ee9a780493eb53400cd4403726
Sean Davis <seandavi@gmail.com>
2024-01-15 15:40:19 -0500

    indentation

--- tests/testthat/test-datastore_datasets.R
+++ tests/testthat/test-datastore_datasets.R
@@ -1,15 +1,15 @@
 TEST_DATASET_ID <- "32ce33c0-a528-4e14-8224-17bc980409cc"
 
 test_that("returns an empty list when no datasets are found in the base path", {
-    # Set up
-    base_path <- system.file(
-        "testdata/datastore",
-        package = "RefineBio"
-    )
+  # Set up
+  base_path <- system.file(
+    "testdata/datastore",
+    package = "RefineBio"
+  )
 
-    # Test
-    result <- datastore_datasets(base_path)
+  # Test
+  result <- datastore_datasets(base_path)
 
-    # Assertion
-    expect_equal(result, TEST_DATASET_ID)
+  # Assertion
+  expect_equal(result, TEST_DATASET_ID)
 })
